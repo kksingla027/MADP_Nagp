632d726aa260093ba6e986f9e9f6b941fb70a265
Kamal Kant Singla <kamal.singla@nagarro.com>
2018-07-29 23:26:41 +0530

    crash report for android added

--- QuizApp/android/app/src/main/java/com/quizapp/MainApplication.java
+++ QuizApp/android/app/src/main/java/com/quizapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.quizapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativePackage(MainApplication.this),
             new AppCenterReactNativePushPackage(MainApplication.this)
       );
     }
